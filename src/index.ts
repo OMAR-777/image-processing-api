5a35bb0129b0f443c05a080dda26ac85dee602b1
OMAR-777 <omar.omar5745@gmail.com>
2022-10-06 19:55:37 +0300

    adding eslint + prettier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Welcome!!")
-})
+  res.send('Welcome!!');
+});
 
 app.listen(port, () => {
-    console.log(`listening on port: ${port}`);
-})
+  console.log(`listening on port: ${port}`);
+});
